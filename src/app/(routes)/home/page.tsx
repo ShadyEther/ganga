23d1b6da23e1ec855ca7237c8dba733de11892e3
ShadyEther <89960953+ShadyEther@users.noreply.github.com>
2024-05-26 17:33:31 +0530

    first commit

---
+++ src/app/(routes)/home/page.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+
+
+
+
+export default function main(){
+    return (
+        <div>
+            This is home page
+            <Typography variant="h5">hehe</Typography>
+            
+        </div>
+    )
+}
